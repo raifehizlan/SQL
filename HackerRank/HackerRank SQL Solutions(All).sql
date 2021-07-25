3f41dc5b219282d186711d4a36ebc91d557e8294
raifehizlan <raifehizlan@gmail.com>
2021-07-25 22:34:38 +0300

    Hackerrank SQL solutions all

---
+++ HackerRank/HackerRank SQL Solutions(All).sql
@@ -0,0 +1,242 @@
+/*
+Revising the Select Query I
+Query all columns for all American cities in the CITY table with populations larger than 100000. 
+The CountryCode for America is USA.
+*/
+SELECT *
+FROM CITY
+WHERE POPULATION >100000
+AND COUNTRYCODE ='USA'
+
+/*
+Revising the Select Query II
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+The CountryCode for America is USA.
+*/
+SELECT NAME
+FROM CITY
+WHERE POPULATION > 120000
+AND COUNTRYCODE = 'USA';
+
+/*
+Select All
+Query all columns (attributes) for every row in the CITY table.
+*/
+SELECT *
+FROM CITY;
+
+/*
+Select By ID
+Query all columns for a city in CITY with the ID 1661.
+*/
+SELECT *
+FROM CITY
+WHERE ID = 1661
+
+/*
+Japanese Cities' Attributes
+Query all attributes of every Japanese city in the CITY table. 
+The COUNTRYCODE for Japan is JPN.
+*/
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+
+/*
+Japanese Cities' Names
+Query the names of all the Japanese cities in the CITY table. 
+The COUNTRYCODE for Japan is JPN.
+*/
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+
+/*
+Weather Observation Station 1
+Query a list of CITY and STATE from the STATION table.
+*/
+SELECT CITY, STATE
+FROM STATION
+
+/*
+Weather Observation Station 3
+Query a list of CITY names from STATION for cities that have an even ID number. 
+Print the results in any order, but exclude duplicates from the answer.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2=0
+
+/*
+Weather Observation Station 4
+Find the difference between the total number of CITY entries in the table 
+and the number of distinct CITY entries in the table.
+*/
+SELECT COUNT(CITY) - COUNT(DISTINCT(CITY))
+FROM STATION
+
+/*
+Weather Observation Station 5
+Query the two cities in STATION with the shortest and longest CITY names, 
+as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, 
+choose the one that comes first when ordered alphabetically.
+*/
+SELECT TOP 1 CITY, LEN(CITY)
+FROM STATION
+ORDER BY LEN(CITY) ASC, CITY; 
+
+SELECT TOP 1 CITY, LEN(CITY)
+FROM STATION
+ORDER BY LEN(CITY) DESC, CITY;
+
+/*
+Weather Observation Station 6
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN ('A','E','I','O','U')
+
+/*
+Weather Observation Station 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) IN ('a','e','i','o','u')
+
+/*
+Weather Observation Station 8
+Query the list of CITY names from STATION which have vowels 
+(i.e., a, e, i, o, and u) as both their first and last characters. 
+Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN ('A','E','I','O','U') 
+AND RIGHT(CITY,1) IN ('a','e','i','o','u')
+
+/*
+Weather Observation Station 9
+Query the list of CITY names from STATION that do not start with vowels.
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U')
+
+/*
+Weather Observation Station 10
+Query the list of CITY names from STATION that do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+/*
+Weather Observation Station 11
+Query the list of CITY names from STATION that either do not start with vowels
+or do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') 
+OR RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+/*
+Weather Observation Station 12
+Query the list of CITY names from STATION that do not start with vowels 
+and do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') 
+AND RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+/*
+Higher Than 75 Marks
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters 
+(i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS >75
+ORDER BY RIGHT(NAME,3), ID
+
+/*
+Employee Names
+Write a query that prints a list of employee names (i.e.: the name attribute)
+from the Employee table in alphabetical order.
+*/
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME;
+
+/*
+Employee Salaries
+Write a query that prints a list of employee names 
+(i.e.: the name attribute) for employees in Employee 
+having a salary greater than $2000 per month 
+who have been employees for less than  months. 
+Sort your result by ascending employee_id.
+*/
+SELECT NAME
+FROM EMPLOYEE
+WHERE SALARY >2000
+AND MONTHS < 10
+ORDER BY EMPLOYEE_ID;
+
+/*
+Type of Triangle
+Write a query identifying the type of each record in the TRIANGLES
+table using its three side lengths. 
+Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+SELECT
+    CASE WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES;
+
+/*
+The PADS
+Generate the following two result sets:
+Query an alphabetically ordered list of all names in OCCUPATIONS, 
+immediately followed by the first letter of each profession as a parenthetical 
+(i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. 
+Sort the occurrences in ascending order, and output them in the following format:
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS 
+and [occupation] is the lowercase occupation name. 
+If more than one Occupation has the same [occupation_count], 
+they should be ordered alphabetically.
+Note: There will be at least two entries in the table for each type of occupation.
+*/
+SELECT Name + '(' + LEFT(OCCUPATION,1) + ')'
+FROM OCCUPATIONS 
+ORDER BY Name;
+
+SELECT 'There are a total of ',  COUNT(OCCUPATION), LOWER(OCCUPATION)+'s.'
+FROM OCCUPATIONS 
+GROUP BY OCCUPATION 
+ORDER BY COUNT(OCCUPATION),LOWER(OCCUPATION);
+
+
