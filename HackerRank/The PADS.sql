43ff167e4c3679a7e6d0580e05e983a494004315
raifehizlan <raifehizlan@gmail.com>
2021-07-25 21:47:50 +0300

    deleted bikestores files

---
+++ HackerRank/The PADS.sql
@@ -0,0 +1,24 @@
+/*
+The PADS
+Generate the following two result sets:
+Query an alphabetically ordered list of all names in OCCUPATIONS, 
+immediately followed by the first letter of each profession as a parenthetical 
+(i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. 
+Sort the occurrences in ascending order, and output them in the following format:
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS 
+and [occupation] is the lowercase occupation name. 
+If more than one Occupation has the same [occupation_count], 
+they should be ordered alphabetically.
+Note: There will be at least two entries in the table for each type of occupation.
+*/
+SELECT Name + '(' + LEFT(OCCUPATION,1) + ')'
+FROM OCCUPATIONS 
+ORDER BY Name;
+
+SELECT 'There are a total of ',  COUNT(OCCUPATION), LOWER(OCCUPATION)+'s.'
+FROM OCCUPATIONS 
+GROUP BY OCCUPATION 
+ORDER BY COUNT(OCCUPATION),LOWER(OCCUPATION);
